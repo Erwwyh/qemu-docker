fbf6e065b551bad1162585103ea5c8352fa6362b
Erwwyh <91678266+Erwwyh@users.noreply.github.com>
2024-08-27 19:04:02 +0800

    Update reset.sh
    err 11 ignor

--- src/reset.sh
+++ src/reset.sh
@@ -7,7 +7,7 @@ warn () { printf "%b%s%b" "\E[1;31m❯ " "Warning: ${1:-}" "\E[0m\n" >&2; }
 
 trap 'error "Status $? while: $BASH_COMMAND (line $LINENO/$BASH_LINENO)"' ERR
 
-[ ! -f "/run/entry.sh" ] && error "Script must run inside Docker container!" && exit 11
+#[ ! -f "/run/entry.sh" ] && error "Script must run inside Docker container!" && exit 11
 [ "$(id -u)" -ne "0" ] && error "Script must be executed with root privileges." && exit 12
 
 echo "❯ Starting $APP for Docker v$(</run/version)..."
